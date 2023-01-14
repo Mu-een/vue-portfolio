58e9fb142b0dead2d5f8fc7305827468a426a830
Mu-een <mueenslam@gmail.com>
2023-01-13 21:55:54 -0800

    update7

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,27 +1,31 @@
 <template>
-    <form action="https://formspree.io/f/xjvdvznl" method="POST" class="w-75 m-5 mx-auto text-center form">
-        <div class="mb-3 row">
-            <div class="col">
-                <label for="fname" class="form-label fname">First Name</label>
-                <input type="text" name="name" class="form-control" placeholder="Type your name here .." required>
-            </div>
-            <div class="col">
-                <label for="lname" class="form-label lname">Last Name</label>
-                <input type="text" name="lastName" class="form-control" placeholder="Type your last name here .." required>
-            </div>
-     </div>
-     <div class="mb-3">
-       <label for="message" class="form-label msgBox">Message</label>
-       <textarea class="form-control" rows="3" placeholder="Type your message here .." required></textarea>
-     </div>
-     <div>
-        <button type="submit button">Send <i class="bi bi-arrow-right"></i></button>
-     </div>
-    </form>
+    <div class="container-fluid">
+        <form action="https://formspree.io/f/xjvdvznl" method="POST" role="form" class="w-75 m-5 mx-auto text-center form">
+            <div class="mb-3 row">
+                <div class="col-md-6 form-group">
+                    <label for="fname" class="form-label fname">First Name</label>
+                    <input type="text" name="name" class="form-control" placeholder="Type your name here .." required="required" data-error="First Name is needed">
+                    <div class="help-block with-errors"></div>
+                </div>
+                <div class="col-md-6 form-group">
+                    <label for="lname" class="form-label lname">Last Name</label>
+                    <input type="text" name="lastName" class="form-control" placeholder="Type your last name here .." required="required" data-error="Last is needed">
+                    <div class="help-block with-errors"></div>
+                </div>
+         </div>
+         <div class="mb-3">
+           <label for="message" class="form-label msgBox">Message</label>
+           <textarea class="form-control" rows="3" placeholder="Type your message here .." required="required"></textarea>
+         </div>
+         <div>
+            <button type="submit button">Send <i class="bi bi-arrow-right"></i></button>
+         </div>
+        </form>
+    </div>
 
-    <div class="socials">
-        <i class="bi bi-geo-alt text-light location-icon d-flex"> Cape Town, South Africa</i>
-        <i class="bi bi-envelope text-light email-icon d-flex">mueenslam@gmail.com</i>
+    <div class="socials text-center mx-auto container">
+        <i class="bi bi-geo-alt text-light location-icon "> Cape Town, South Africa</i>
+        <i class="bi bi-envelope text-light email-icon d-block">mueenslam@gmail.com</i>
         <a href="https://www.linkedin.com/in/mueenslamat" target="_blank"><i class="bi bi-linkedin linkedin-icon"></i></a>
         <a href="https://twitter.com/mueenslamat" target="_blank"><i class="bi bi-twitter twitter-icon"></i></a>
         <a href="https://www.instagram.com/mueenslamat/" target="_blank"><i class="bi bi-instagram instagram-icon"></i></a>
@@ -66,11 +70,17 @@ button:hover {
 .linkedin-icon, .twitter-icon, .instagram-icon{
     color: #737ccf;
     font-size: 2rem;
+    margin-right: 20px;
+    margin-left: 30px;
 }
 
 .linkedin-icon:hover , .twitter-icon:hover, .instagram-icon:hover{
     color:yellow;
     cursor: pointer;
 }
-
+@media screen and (min-width:200px) {
+    .socials i {
+        font-size: 20px;
+    }
+}
 </style>
